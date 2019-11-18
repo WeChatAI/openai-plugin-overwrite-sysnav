b197eb49742d013b146f20a4f2a09b835258b399
v_hayang <v_hayang@tencent.com>
2019-11-18 15:15:40 +0800

    增加h5卡片

---
+++ pages/webViewPage/webViewPage.js
@@ -0,0 +1,40 @@
+//Page Object
+Page({
+  data: {
+    url: ''
+  },
+  //options(Object)
+  onLoad: function(options){
+    this.setData({
+      url: options.url
+    })
+  },
+  onReady: function(){
+    
+  },
+  onShow: function(){
+    
+  },
+  onHide: function(){
+
+  },
+  onUnload: function(){
+
+  },
+  onPullDownRefresh: function(){
+
+  },
+  onReachBottom: function(){
+
+  },
+  onShareAppMessage: function(res){
+   
+  },
+  onPageScroll: function(){
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
+
+  }
+});
